8e5bacf5dfa4b4df56d9b5a13e3f9aa6c0665cd4
PiniduInduwara <it20202286@my.sliit.lk>
2022-04-27 09:59:42 +0530

    Initial Commit

---
+++ src/main/java/com/MIniProject/MIniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.MIniProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MIniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MIniProjectApplication.class, args);
+	}
+
+}
