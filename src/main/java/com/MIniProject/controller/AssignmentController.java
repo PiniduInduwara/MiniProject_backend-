dc5838780bf26afabf900d45c46ead950792916c
PiniduInduwara <it20202286@my.sliit.lk>
2022-05-02 09:34:23 +0530

    Commit

--- src/main/java/com/MIniProject/controller/AssignmentController.java
+++ src/main/java/com/MIniProject/controller/AssignmentController.java
@@ -6,20 +6,37 @@ import com.MIniProject.service.AssignmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class AssignmentController {
 
     @Autowired
     AssignmentService assignmentService;
 
+    @GetMapping("/assignment")
+    public List<Assignment> getAllAssignments(){
+        return assignmentService.getAllAssignments();
+    }
+
+    @PostMapping("/assignment/{assignmentId}")
+    public Assignment getAssignmentsById(@PathVariable("/assignemnt/{assignmentId}") String id){
+        return assignmentService.getAssignmentsById(id);
+    }
+
     @PostMapping("/assignment")
     public Assignment addAssignment(@RequestBody AssignmentCreate assignmentCreate){
         return assignmentService.addAssignment(assignmentCreate);
     }
 
-    @PutMapping("/assignment/{assignmentId}")
+    @PutMapping("/status/{assignmentId}")
     public Assignment changeStatus(@PathVariable("assignmentId") String id, @RequestBody Assignment assignment){
         return assignmentService.changeStatus(assignment, id);
+    }
+
+    @PutMapping("/assignment/{assignmentId}")
+    public Assignment changeAssignment(@PathVariable("assignmentId")String id, @RequestBody AssignmentCreate assignmentCreate){
+        return assignmentService.changeAssignment(assignmentCreate, id);
     }
 
     @DeleteMapping("/assignment/{assignmentId}")
