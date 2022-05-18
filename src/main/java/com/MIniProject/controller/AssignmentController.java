ffa85258ccf1c8dcf1dcae51331acaf7c814c811
PiniduInduwara <it20202286@my.sliit.lk>
2022-05-18 11:19:56 +0530

    Commit

--- src/main/java/com/MIniProject/controller/AssignmentController.java
+++ src/main/java/com/MIniProject/controller/AssignmentController.java
@@ -7,6 +7,7 @@ import com.MIniProject.service.AssignmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -21,27 +22,27 @@ public class AssignmentController {
     }
 
     @GetMapping("/assignment/{assignmentId}")
-    public Assignment getAssignmentsById(@PathVariable("assignmentId") String id){
+    public Assignment getAssignmentsById(@Valid @PathVariable("assignmentId") String id){
         return assignmentService.getAssignmentsById(id);
     }
 
     @PostMapping("/assignment")
-    public Assignment addAssignment(@RequestBody Assignment assignment){
+    public Assignment addAssignment(@Valid @RequestBody Assignment assignment){
         return assignmentService.addAssignment(assignment);
     }
 
     @PutMapping("/status/{assignmentId}")
-    public Assignment changeStatus(@PathVariable("assignmentId") String id, @RequestBody Assignment assignment){
+    public Assignment changeStatus(@Valid @PathVariable("assignmentId") String id, @RequestBody Assignment assignment){
         return assignmentService.changeStatus(assignment, id);
     }
 
     @PutMapping("/assignment/{assignmentId}")
-    public Assignment changeAssignment(@PathVariable("assignmentId")String id, @RequestBody Assignment assignment){
+    public Assignment changeAssignment(@Valid @PathVariable("assignmentId")String id, @RequestBody Assignment assignment){
         return assignmentService.changeAssignment(assignment, id);
     }
 
     @DeleteMapping("/assignment/{assignmentId}")
-    public Assignment deleteAssignment(@PathVariable("assignmentId") String id){
+    public Assignment deleteAssignment(@Valid @PathVariable("assignmentId") String id){
         return assignmentService.deleteAssignment(id);
     }
 }
